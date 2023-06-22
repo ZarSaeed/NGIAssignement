ed9c98a03b75f3c36d20cc42d7c8eb2ab62c303d
zarsaeed <zarafridi1@gmail.com>
2023-06-22 10:34:42 +0500

    page 1 is last page unit test

--- presenter/src/test/java/com/nextgeni/presenter/ui/movielisting/MovieListingViewModelTest.kt
+++ presenter/src/test/java/com/nextgeni/presenter/ui/movielisting/MovieListingViewModelTest.kt
@@ -1,16 +1,42 @@
 package com.nextgeni.presenter.ui.movielisting
 
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.nextgeni.domain.models.MoviesPaginatedResponse
 import com.nextgeni.domain.usecases.GetMovieDetailsUC
+import com.nextgeni.domain.usecases.GetMoviesUC
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
 import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
 
 
+@ExperimentalCoroutinesApi
+@RunWith(AndroidJUnit4::class)
 class MovieListingViewModelTest {
 
     lateinit var viewModel: MovieListingViewModel
-    lateinit var movieListingUC: GetMovieDetailsUC
+    lateinit var getMoviesUC: GetMoviesUC
 
     @Before
     fun setup() {
-
+        getMoviesUC = Mockito.mock(GetMoviesUC::class.java)
+        viewModel = MovieListingViewModel(getMoviesUC)
     }
+
+    @Test
+    fun `given page 1 check all pages loaded`() = runTest {
+        //arrange
+        `when`(getMoviesUC(viewModel.moviesRequest)).thenReturn(flowOf(MoviesPaginatedResponse(page = 1,
+            emptyList(),1,1
+        )))
+        //act
+        viewModel.getMovies()
+        //assert
+        assert(viewModel.listState == MovieListingViewModel.ListState.PAGINATION_EXHAUST)
+    }
+
 }
