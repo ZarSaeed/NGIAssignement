d2ddd3f64e7af55f4ec176a4f2db5c07230f3f56
zarsaeed <zarafridi1@gmail.com>
2023-06-22 10:15:53 +0500

    gitignore update and testing framework

---
+++ presenter/src/test/java/com/nextgeni/presenter/ui/movielisting/MovieListingViewModelTest.kt
@@ -0,0 +1,16 @@
+package com.nextgeni.presenter.ui.movielisting
+
+import com.nextgeni.domain.usecases.GetMovieDetailsUC
+import org.junit.Before
+
+
+class MovieListingViewModelTest {
+
+    lateinit var viewModel: MovieListingViewModel
+    lateinit var movieListingUC: GetMovieDetailsUC
+
+    @Before
+    fun setup() {
+
+    }
+}
